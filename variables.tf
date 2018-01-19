27ac3746155b792c74b809136650f0dccfd2df56
hrudaya nayak <cheftooldemo@gmail.com>
2018-01-19 18:41:02 +0530

    c

--- variables.tf
+++ variables.tf
@@ -1,9 +1,4 @@
 variable "aws_region" {}
 variable "aws_profile" {}
-variable "db_instance_class" {}
-variable "dbname" {}
-variable "dbuser" {}
-variable "dbpassword" {}
-variable "key_name" {}
 variable "public_key_path" {}
 variable "domain_name" {} 
